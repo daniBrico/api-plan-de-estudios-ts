32aa6e50a7659c925204414d765dbf7c62d84ed3
Daniel <daniel.jorge96@outlook.com>
2025-12-01 10:21:02 -0300

    Add verifyToken function to auth controller

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -41,3 +41,37 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: 'Internal server error' })
   }
 }
+
+export const verifyToken = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const _id = req.user?._id
+
+    const userFounded = await UserModel.findById(_id)
+
+    if (!userFounded) {
+      res.status(401).json({ message: 'Authentication error' })
+      return
+    }
+
+    res.status(200).json({
+      message: 'Successful authentication',
+      user: {
+        _id: userFounded._id,
+        name: userFounded.name,
+        lastName: userFounded.lastName,
+        email: userFounded.email,
+      },
+    })
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ message: 'Internal server error' })
+      return
+    }
+
+    // fallback por si err no es una instancia de Error
+    res.status(500).json({ message: 'Unexpected error' })
+  }
+}
