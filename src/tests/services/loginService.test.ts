4ed0b18d939cb067d7a272e592f448db9a7a16cd
Daniel <daniel.jorge96@outlook.com>
2025-12-30 01:24:53 -0300

    Configure Jest and create tests for login and verification services

---
+++ src/tests/services/loginService.test.ts
@@ -0,0 +1,57 @@
+import UserModel from '../../models/mongoDB/schemas/user.model'
+import { loginService } from '../../services/auth.service'
+import bcrypt from 'bcrypt'
+import { VerificationService } from '../../services/verification.service'
+
+jest.mock('../../models/mongoDB/schemas/user.model')
+jest.mock('bcrypt')
+
+jest.mock('../../services/verification.service', () => ({
+  VerificationService: {
+    handleUnverifiedUser: jest.fn(),
+  },
+}))
+
+describe('loginService', () => {
+  it('throws error if user does not exist', async () => {
+    ;(UserModel.findOne as jest.Mock).mockResolvedValue(null)
+
+    await expect(loginService('test@mail.com', '123456')).rejects.toThrow(
+      'Invalid credentials'
+    )
+  })
+
+  it('returns NOT_VERIFIED when user is not verified', async () => {
+    const mockUser = {
+      password: 'hashed',
+      isVerified: false,
+    }
+
+    ;(UserModel.findOne as jest.Mock).mockResolvedValue(mockUser)
+    ;(bcrypt.compare as jest.Mock).mockResolvedValue(true)
+    ;(VerificationService.handleUnverifiedUser as jest.Mock).mockResolvedValue({
+      status: 'EMAIL_SENT',
+    })
+
+    const result = await loginService('test@email.com', '123456')
+
+    expect(result.status).toBe('NOT_VERIFIED')
+    expect(VerificationService.handleUnverifiedUser).toHaveBeenCalledWith(
+      mockUser
+    )
+  })
+
+  it('returns OK when the user is registered and verified', async () => {
+    const mockUser = {
+      password: 'hashed',
+      isVerified: true,
+    }
+
+    ;(UserModel.findOne as jest.Mock).mockResolvedValue(mockUser)
+    ;(bcrypt.compare as jest.Mock).mockResolvedValue(true)
+
+    const result = await loginService('test@email.com', '123456')
+
+    expect(result.status).toBe('OK')
+  })
+})
---
+++ src/tests/services/verificationService.test.ts
@@ -0,0 +1,113 @@
+import { Temporal } from '@js-temporal/polyfill'
+import {
+  canSendVerificationEmailReturn,
+  VerificationService,
+} from '../../services/verification.service'
+import { UserDocument } from '../../types/domain/user'
+
+const FIXED_DATE = Temporal.Instant.from('2025-01-01T12:00:00Z')
+
+beforeAll(() => {
+  jest.spyOn(Temporal.Now, 'instant').mockReturnValue(FIXED_DATE)
+
+  jest
+    .spyOn(Temporal.Now, 'plainDateISO')
+    .mockReturnValue(FIXED_DATE.toZonedDateTimeISO('UTC').toPlainDate())
+
+  jest.spyOn(Temporal.Now, 'timeZoneId').mockReturnValue('UTC')
+})
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
+
+const mockUser = () =>
+  ({
+    email: 'test@mail.com',
+    name: 'Test',
+    verificationToken: 'token',
+    verificationTokenExpires: new Date(Date.now() + 10000),
+    verificationEmailAttempts: 0,
+    lastVerificationEmailSentAt: null,
+    save: jest.fn().mockResolvedValue(true),
+  } as unknown as UserDocument)
+
+describe('canSendVerificationEmail', () => {
+  it('Allows sending an email if everything is OK', () => {
+    const user = mockUser()
+
+    const result: canSendVerificationEmailReturn =
+      VerificationService.canSendVerificationEmail(user)
+
+    const expected: canSendVerificationEmailReturn = {
+      action: 'SEND',
+      regenerateToken: false,
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('Block if the daily maximum is reached', () => {
+    const user = mockUser()
+    user.verificationEmailAttempts = 2
+
+    const result = VerificationService.canSendVerificationEmail(user)
+
+    // Debería establecer que no se reinicio el número de intentos
+    // Y debería verificar qué pasa si se reinicia el número máximo de intentos antes de pasar a la línea que se fija si se paso del número máximo establecido por defecto (2)
+    const expected: canSendVerificationEmailReturn = {
+      action: 'BLOCKED',
+      reason: 'MAX_ATTEMPTS_REACHED',
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('The maximum email attempts per day are reset.', () => {
+    const user = mockUser()
+    user.verificationEmailAttempts = 2
+    user.lastVerificationEmailSentAt = new Date('2024-12-31T12:00:00Z')
+    user.verificationTokenExpires = new Date(
+      FIXED_DATE.epochMilliseconds - 1000 * 60 * 60 * 24
+    )
+
+    const result = VerificationService.canSendVerificationEmail(user)
+
+    const expected: canSendVerificationEmailReturn = {
+      action: 'SEND',
+      regenerateToken: true,
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('Block if trying to resend too quickly', () => {
+    const user = mockUser()
+    // Five minutes before
+    user.lastVerificationEmailSentAt = new Date('2025-01-01T11:55:00Z')
+
+    const result = VerificationService.canSendVerificationEmail(user)
+
+    const expected: canSendVerificationEmailReturn = {
+      action: 'BLOCKED',
+      reason: 'TOO_SOON',
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('Generate token if it does not exist', () => {
+    const user = mockUser()
+    user.verificationToken = null
+    user.verificationTokenExpires = null
+
+    const result = VerificationService.canSendVerificationEmail(user)
+
+    const expected: canSendVerificationEmailReturn = {
+      action: 'SEND',
+      regenerateToken: true,
+    }
+
+    expect(result).toEqual(expected)
+  })
+})
