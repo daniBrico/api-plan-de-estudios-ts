a4a28fa782d74e8aeeb81dde69ab7e623de87883
Daniel <daniel.jorge96@outlook.com>
2026-01-25 00:19:27 -0300

    Change same site option for production and development

--- src/utils/authCookies.ts
+++ src/utils/authCookies.ts
@@ -5,7 +5,7 @@ export const setAuthCookie = (res: Response, token: string) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: ENV.IS_PRODUCTION,
-    sameSite: 'none',
+    sameSite: ENV.IS_PRODUCTION ? 'none' : 'lax',
     path: '/',
     maxAge: Number(ENV.TOKEN_MAX_AGE),
   })
@@ -15,7 +15,7 @@ export const clearAuthCookie = (res: Response) => {
   res.clearCookie('token', {
     httpOnly: true,
     secure: ENV.IS_PRODUCTION,
-    sameSite: 'none',
+    sameSite: ENV.IS_PRODUCTION ? 'none' : 'lax',
     path: '/',
   })
 }
