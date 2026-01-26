0866b613380b399e4112cb84eabbba463dd382b0
Daniel <daniel.jorge96@outlook.com>
2026-01-26 17:23:22 -0300

    Add email provider factory based a string prop

---
+++ src/emails/email.factory.ts
@@ -0,0 +1,13 @@
+import { EmailProvider } from './email.provider'
+import { BrevoEmailProvider } from './providers/brevo.provider'
+import { ResendEmailProvider } from './providers/resend.provider'
+
+export const createEmailProvider = (emailProvider: string): EmailProvider => {
+  switch (emailProvider) {
+    case 'resend':
+      return new ResendEmailProvider()
+    case 'brevo':
+    default:
+      return new BrevoEmailProvider()
+  }
+}
